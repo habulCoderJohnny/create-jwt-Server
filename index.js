2ed8d977c52ba3cbff6c60e2256e15729d0803a6
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-12 13:05:42 +0600

    get user data from client

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
-
 //log 
 app.get('/', (req,res)=>{
     res.send('Running Jwt token server...');
 });
+
+app.post('/login', (req, res)=>{
+    const user = req.body;
+    console.log(user);
+    // DANGER: Do not check password here for serious application
+    // USE proper process for hashing and checking
+    res.send({'success': true});
+})
 
 //log 
 app.listen(port,()=>{
